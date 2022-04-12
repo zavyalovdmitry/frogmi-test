75a3db5ae0ff7917e61f375ac5d76d6a5242b0d7
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2022-04-12 15:57:48 -0400

    finish task

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# frogmi-test
+# Frogmi test task
+
+## `Store`
+
+A simple class for Incidents.
+
+### Properties
+
+- `storeName {String}` a name of an instance
+- `incidents {Array of Object}` an array of incidents
+  - `openDate {Date}` a date of an incident
+  - `closeDate {Date}` a date of solving the incident
+  - `description {String}` a description of the incident
+  - `actionNeeded {String}` a description of an action needed to solve the incident
+  - `status {String}` a status of the incident: `open` - for active incident, `solved` - for closed incident
+
+### Methods
+
+#### open
+
+Adds a new incident
+
+_Parameters_
+
+- `description {String}` a description of an incident
+- `actionNeeded {String}` a description of an action needed to solve the incident
+- `date {Date}` a date of an incident, **not required, default value is current date**
+
+_Returns_
+
+- `{Number}` an id of added incident
+
+#### close
+
+Changes a status of an incident to solved
+
+_Parameters_
+
+- `id {Number}` an id of an incident to close
+- `date {Date}` a date of closing the incident, **not required, default value is current date**
+
+#### incidentStatus
+
+Calculates statistics on the incidents between two dates:
+
+- the number of “open” cases between those dates
+- the number of “solved” cases between those dates
+- the average solution time between those dates (including only the solved cases)
+- the current maximum solution time between those dates (include open cases using the current time).
+
+_Parameters_
+
+- `fromDate {Date}` a starting date (YYYY-MM-DD) of an incidents, **not required, default value is minimum date from all incidents**
+- `toDate {Date}` the last date (YYYY-MM-DD) of an incidents, **not required, default value is current date**
+
+_Returns_
+
+- `{String}` statistics on the incidents between given dates
+
+## `storeTestInit`
+
+A function for initializing an instance of a Store class with some test data.
+
+_Parameters_
+
+- `storeName {Object}` an instance of a Store class
--- frogmi.js
+++ frogmi.js
@@ -1,63 +1,32 @@
 class Store {
-  incidents = [
-    {
-      openDate: '2012-01-26T13:51:50.417-07:00',
-      closeDate: '2012-01-26T15:51:50.417-07:00',
-      description: 'the floor in the fruit area is dirty',
-      status: 'solved',
-    },
-    {
-      openDate: '2012-01-26T13:45:50.417-07:00',
-      closeDate: '2012-01-26T15:47:50.417-07:00',
-      description: 'the floor in the fruit area is dirty',
-      status: 'solved',
-    },
-    {
-      openDate: '2012-01-25T13:51:50.417-07:00',
-      closeDate: '2012-01-26T15:51:50.417-07:00',
-      description: 'the floor in the fruit area is dirty, like a looooot!',
-      status: 'open',
-    },
-  ];
-
-  constructor(storeName) {
+  constructor(storeName = 'A store') {
     this.storeName = storeName;
+    this.incidents = [];
   }
 
-  get storeName() {
-    return this._storeName;
-  }
-
-  set storeName(value) {
-    if (value.length < 4) {
-      alert('Description is too short.');
-      return;
-    }
-    this._storeName = value;
-  }
-
-  open(description) {
+  open(description, actionNeeded, date = new Date()) {
     this.incidents.push({
-      openDate: new Date(),
+      openDate: date,
       closeDate: '',
       description,
+      actionNeeded,
       status: 'open',
     });
 
     return this.incidents.length - 1;
   }
 
-  close(id) {
-    this.incidents[id].status = 'closed';
-    this.incidents[id].closeDate = new Date();
+  close(id, date = new Date()) {
+    this.incidents[id].status = 'solved';
+    this.incidents[id].closeDate = date;
   }
 
   openCases(fromDate, toDate) {
     return this.incidents.filter(
       (item) =>
         item.status === 'open' &&
-        item.openDate >= fromDate &&
-        item.openDate <= toDate
+        item.openDate >= new Date(fromDate) &&
+        item.openDate <= new Date(toDate)
     );
   }
 
@@ -65,53 +34,101 @@ class Store {
     return this.incidents.filter(
       (item) =>
         item.status === 'solved' &&
-        item.openDate >= fromDate &&
-        item.closeDate <= toDate
+        item.openDate >= new Date(fromDate) &&
+        item.openDate <= new Date(toDate)
     );
   }
 
   averageSolution(fromDate, toDate) {
     const cases = this.closedCases(fromDate, toDate);
-    // console.log(cases);
     const time = cases.reduce(
-      (sum, current) =>
-        sum + (new Date(current.closeDate) - new Date(current.openDate)),
+      (sum, current) => sum + (current.closeDate - current.openDate),
       0
     );
     return Math.floor(time / 1000 / 60 / cases.length);
   }
 
   maximumSolution(fromDate, toDate) {
-    const cases = this.closedCases(fromDate, toDate);
-    // console.log(cases);
-    const time = cases.reduce(
-      (max, current) =>
-        max < new Date(current.closeDate) - new Date(current.openDate)
-          ? new Date(current.closeDate) - new Date(current.openDate)
-          : max,
-      0
+    const cases = this.incidents.map((item) =>
+      item.status === 'open' ? { ...item, closeDate: new Date() } : item
     );
-    return Math.floor(time / 1000 / 60 / cases.length);
+    const casesFiltered = cases.filter(
+      (item) =>
+        item.openDate >= new Date(fromDate) && item.openDate <= new Date(toDate)
+    );
+    const time = casesFiltered.reduce(
+      (max, current) =>
+        max < current.closeDate - current.openDate
+          ? current.closeDate - current.openDate
+          : max,
+      this.incidents[0].closeDate - this.incidents[0].openDate
+    );
+
+    return Math.floor(time / 1000 / 60);
   }
 
-  incidentStatus(fromDate, toDate) {
+  getMinDate() {
+    return this.incidents.reduce(
+      (min, current) => (min > current.openDate ? current.openDate : min),
+      this.incidents[0].openDate
+    );
+  }
+
+  incidentStatus(fromDate = '', toDate = new Date()) {
+    fromDate = fromDate ? fromDate : this.getMinDate();
+
     console.log(
-      `open_cases: ${this.openCases(fromDate, toDate).length}, closed_cases: ${
+      `Open cases: ${this.openCases(fromDate, toDate).length}, closed cases: ${
         this.closedCases(fromDate, toDate).length
-      }, average_solution: ${this.averageSolution(
+      }, average solution time: ${this.averageSolution(
         fromDate,
         toDate
-      )}, maximum_solution: ${this.maximumSolution(fromDate, toDate)}`
+      )} min, maximum solution time: ${this.maximumSolution(
+        fromDate,
+        toDate
+      )} min`
     );
   }
 }
 
-const someStoreTestInit = (storeName) => {};
+const storeTestInit = (storeName) => {
+  storeName.open(
+    'the floor in the fruit area is dirty',
+    'cleaning',
+    new Date('2022-01-26T13:51:50.417-07:00')
+  );
+  storeName.open(
+    'the ceil in the fruit area is dirty',
+    'cleaning',
+    new Date('2022-01-26T13:45:50.417-07:00')
+  );
+  storeName.open(
+    'the floor in the fruit area is dirty, like a looooot!',
+    'cleaning',
+    new Date('2022-01-25T13:51:50.417-07:00')
+  );
+  storeName.open(
+    'the floor in the fruit area is dirty',
+    'cleaning',
+    new Date('2022-01-25T11:51:50.417-07:00')
+  );
+  storeName.open(
+    'the ceil in the fruit area is dirty',
+    'cleaning',
+    new Date('2022-01-26T13:32:50.417-07:00')
+  );
+  storeName.open(
+    'the floor in the fruit area is dirty, like a looooot!',
+    'cleaning',
+    new Date('2022-01-25T13:01:50.417-07:00')
+  );
+
+  storeName.close(1, new Date('2022-01-26T15:51:50.417-07:00'));
+  storeName.close(3, new Date('2022-01-25T11:57:50.417-07:00'));
+};
 
 const someStore = new Store('someStore');
-someStoreTestInit(someStore);
 
-someStore.incidentStatus(
-  '2012-01-25T13:51:50.417-07:00',
-  '2012-01-26T15:51:50.417-07:00'
-);
+storeTestInit(someStore);
+
+someStore.incidentStatus();
